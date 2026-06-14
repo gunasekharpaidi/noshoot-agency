9828554f62e7e12654c00bb4d601f2a452b5833f
build100tech <build100tech@gmail.com>
2026-06-15 00:51:19 +0530

    feat: add brand colour tokens to Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ module.exports = {
     './components/**/*.{js,jsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          black: '#0C0C0C',
+          white: '#F8F7F3',
+          red:   '#E8300A',
+          muted: '#8A8A82',
+          card:  '#EDECE8',
+        },
+      },
+      fontFamily: {
+        condensed: ['var(--font-barlow)', 'sans-serif'],
+        sans:      ['var(--font-dm-sans)', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
